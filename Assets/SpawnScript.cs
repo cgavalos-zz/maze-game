60bbec5d4dcee73a2497803a3e00e61b7c0a1be1
Carlos G Avalos <carlos.g.avalos@gmail.com>
2018-03-02 12:06:46 -0800

    Auto-generate anything in Unity

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    cube.transform.position = new Vector3(0, 2, 0);
+  }
+
+  // Update is called once per frame
+  void Update () {
+    //
+  }
+}
