1657cbae1e37c49c1cea1169c1e2cfc95eaa72ff
Carlos G Avalos <carlos.g.avalos@gmail.com>
2018-03-02 12:27:03 -0800

    Spawn cubes with dimensions and positions depending on index

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -6,9 +6,13 @@ public class SpawnScript : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
-    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-    cube.transform.localPosition = new Vector3(0.0f, 2.0f, 0.0f);
-    cube.transform.localScale = new Vector3(2.0f, 0.5f, 4.0f);
+
+    for (int i = 0; i < 10; i++) {
+      GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+      cube.transform.localPosition = new Vector3(1.1f * i, 2.0f, 0.0f);
+      cube.transform.localScale = new Vector3(1.0f, 0.1f * (i + 1), 1.0f);
+      cube.AddComponent<Rigidbody>();
+    }
   }
 
   // Update is called once per frame
